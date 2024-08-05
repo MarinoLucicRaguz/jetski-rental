dae48664289d1f5da89ba2ab203e01194492e068
MarinoLucicRaguz <marinolucicraguz@gmail.com>
2024-08-05 18:17:07 +0200

    checking do we have information of date

--- actions/listReservationsForDate.ts
+++ actions/listReservationsForDate.ts
@@ -4,6 +4,7 @@ import { fetchReservationsByDate } from "@/data/reservationData";
 import { DateTime } from "luxon";
 
 export const listReservationsByDate = async (date: Date) => {
+    console.log("Checking information: ", date)
     const nextDayDate = DateTime.fromJSDate(date).plus({ days: 1 }).toJSDate();
     
     const reservationsByDate = await fetchReservationsByDate(nextDayDate);
