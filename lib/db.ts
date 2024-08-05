be5fde8c28075ba35723a3471796155aa1b0c7d5
MarinoLucicRaguz <marinolucicraguz@gmail.com>
2024-08-05 15:02:35 +0200

    prisma client produkcija

--- lib/db.ts
+++ lib/db.ts
@@ -4,10 +4,15 @@ declare global {
     var prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export const db = new PrismaClient();
 
 //u normalnoj situaciji samo bi bilo PrismaClient(), ali ovo je zato sto smo u produkciji, pa ne zelimo da se stalno stvaraju
 //export const db= new PrismaClient()
+
+// export const db = globalThis.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+
+// //u normalnoj situaciji samo bi bilo PrismaClient(), ali ovo je zato sto smo u produkciji, pa ne zelimo da se stalno stvaraju
+// //export const db= new PrismaClient()
 
