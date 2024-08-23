28c0fb935f4ecd6c048037402a11d615e24b5909
MarinoLucicRaguz <marinolucicraguz@gmail.com>
2024-08-24 00:10:45 +0200

    popravljanje za produkciju

--- actions/calculateAvailability.ts
+++ actions/calculateAvailability.ts
@@ -1,9 +1,9 @@
-"use server";
+'use server';
 
-const moment = require('moment-timezone')
+const moment = require('moment-timezone');
 
-import { db } from "@/lib/db";
-import { RentalOptions } from "@prisma/client";
+import { db } from '@/lib/db';
+import { RentalOptions } from '@prisma/client';
 
 interface AvailabilitySlot {
   start_time: string;
@@ -17,17 +17,20 @@ const BUFFER_MINUTES = 5;
 const generateDynamicSlots = (
   duration: number,
   startTime: Date,
-  endTime: Date,
-): { start: Date, end: Date }[] => {
+  endTime: Date
+): { start: Date; end: Date }[] => {
   const slots = [];
-  const now = moment().tz("UTC");
+  const now = moment().tz('UTC');
 
-  let start = moment(startTime).tz("UTC");
-  const end = moment(endTime).tz("UTC");
+  let start = moment(startTime).tz('UTC');
+  const end = moment(endTime).tz('UTC');
 
-  console.log("Početak i kraj generiranja slotova: ", start,end)
+  console.log('Početak i kraj generiranja slotova: ', start, end);
   if (start.isSame(now, 'day') && now.isAfter(start)) {
-    start = now.clone().minute(Math.ceil(now.minute() / 5) * 5).second(0);
+    start = now
+      .clone()
+      .minute(Math.ceil(now.minute() / 5) * 5)
+      .second(0);
   }
 
   while (start.isBefore(end) || start.isSame(end)) {
@@ -47,13 +50,17 @@ export const calculateAvailability = async (
   dayEndTime: Date,
   jetskiCount: number,
   rentalOption: RentalOptions,
-  location?: number,
+  location?: number
 ): Promise<AvailabilitySlot[]> => {
-  console.log("Calculate availability - StartTime: ", dayStartTime)
-  console.log("Calculate availability - EndTime: " , dayEndTime)
-  
-  const slots = generateDynamicSlots(rentalOption.duration, dayStartTime, dayEndTime);
-  console.log(slots[slots.length-1]);
+  console.log('Calculate availability - StartTime: ', dayStartTime);
+  console.log('Calculate availability - EndTime: ', dayEndTime);
+
+  const slots = generateDynamicSlots(
+    rentalOption.duration,
+    dayStartTime,
+    dayEndTime
+  );
+  console.log(slots[slots.length - 1]);
   const reservations = await db.reservation.findMany({
     where: {
       startTime: {
@@ -65,12 +72,11 @@ export const calculateAvailability = async (
       reservation_jetski_list: true,
     },
   });
-  
-  
+
   const availabilitySlots: AvailabilitySlot[] = [];
 
   let numberOfAvailableVehicles;
-  
+
   if (!location) {
     numberOfAvailableVehicles = await db.jetski.count({
       where: {
@@ -86,24 +92,34 @@ export const calculateAvailability = async (
     });
   }
 
-  if (rentalOption.rentaloption_description === "SAFARI") {
+  if (rentalOption.rentaloption_description === 'SAFARI') {
     jetskiCount += 1;
   }
 
   reservations.forEach((reservation) => {
     const reservationStartTime = new Date(reservation.startTime);
     const reservationEndTime = new Date(reservation.endTime);
-    const reservationEndWithBuffer = new Date(reservationEndTime.getTime() + BUFFER_MINUTES * 60 * 1000);
+    const reservationEndWithBuffer = new Date(
+      reservationEndTime.getTime() + BUFFER_MINUTES * 60 * 1000
+    );
 
     slots.forEach((slot, index) => {
       if (
-        reservationStartTime < new Date(slot.end.getTime() + BUFFER_MINUTES * 60 * 1000) &&
+        reservationStartTime <
+          new Date(slot.end.getTime() + BUFFER_MINUTES * 60 * 1000) &&
         reservationEndWithBuffer > slot.start
       ) {
-        const locationFiltered = !location || reservation.reservation_jetski_list.some(jetski => jetski.jetski_location_id === location);
+        const locationFiltered =
+          !location ||
+          reservation.reservation_jetski_list.some(
+            (jetski) => jetski.jetski_location_id === location
+          );
         const reservedJetskis = reservation.reservation_jetski_list.length;
 
-        if (locationFiltered && (numberOfAvailableVehicles - reservedJetskis < jetskiCount)) {
+        if (
+          locationFiltered &&
+          numberOfAvailableVehicles - reservedJetskis < jetskiCount
+        ) {
           slots.splice(index, 1); // Remove this slot
         }
       }
@@ -111,59 +127,59 @@ export const calculateAvailability = async (
   });
 
   // Convert remaining slots to AvailabilitySlot format
-  slots.forEach(slot => {
-    availabilitySlots.push(slot => ({
-      start_time: slot.start.toTimeString(),
-      end_time: slot.end.toTimeString(),
-      available_jetskis: numberOfAvailableVehicles, 
-    }))
-  });
-  slots.map(slot => ({
-    start_time: slot.start.toTimeString(),
-    end_time: slot.end.toTimeString(),
-    available_jetskis: numberOfAvailableVehicles,  // Assuming all remaining slots have the same available jetskis
-  }));
+  // slots.forEach(slot => {
+  //   availabilitySlots.push(slot => ({
+  //     start_time: slot.start.toTimeString(),
+  //     end_time: slot.end.toTimeString(),
+  //     available_jetskis: numberOfAvailableVehicles,
+  //   }))
+  // });
+  // slots.map(slot => ({
+  //   start_time: slot.start.toTimeString(),
+  //   end_time: slot.end.toTimeString(),
+  //   available_jetskis: numberOfAvailableVehicles,  // Assuming all remaining slots have the same available jetskis
+  // }));
 
   return availabilitySlots;
 };
-  
-  // let currentSlotIndex = 0;
-  // while (currentSlotIndex < slots.length) {
-  //   const slot = slots[currentSlotIndex];
-  //   const slotEndWithBuffer = new Date(slot.end.getTime() + BUFFER_MINUTES * 60 * 1000);
 
-  //   const overlappingReservations = reservations.filter((reservation) => {
-  //     const reservationStartTime = new Date(reservation.startTime);
-  //     const reservationEndTime = new Date(reservation.endTime);
-  //     const reservationEndWithBuffer = new Date(reservationEndTime.getTime() + BUFFER_MINUTES * 60 * 1000);
-    
-  //     const isOverlapping = (
-  //       reservationStartTime < slotEndWithBuffer && reservationEndWithBuffer > slot.start
-  //     );
-    
-  //     return isOverlapping;
-  //   });
-    
-  //   const locationFilteredReservations = overlappingReservations.filter((reservation) => {
-  //     return !location || reservation.reservation_jetski_list.some(jetski => jetski.jetski_location_id === location);
-  //   });
+// let currentSlotIndex = 0;
+// while (currentSlotIndex < slots.length) {
+//   const slot = slots[currentSlotIndex];
+//   const slotEndWithBuffer = new Date(slot.end.getTime() + BUFFER_MINUTES * 60 * 1000);
 
-  //   const reservedJetskis = locationFilteredReservations.reduce((count, reservation) => {
-  //     return count + reservation.reservation_jetski_list.length;
-  //   }, 0);
+//   const overlappingReservations = reservations.filter((reservation) => {
+//     const reservationStartTime = new Date(reservation.startTime);
+//     const reservationEndTime = new Date(reservation.endTime);
+//     const reservationEndWithBuffer = new Date(reservationEndTime.getTime() + BUFFER_MINUTES * 60 * 1000);
 
-  //   const availableJetskisCount = numberOfAvailableVehicles - reservedJetskis;
+//     const isOverlapping = (
+//       reservationStartTime < slotEndWithBuffer && reservationEndWithBuffer > slot.start
+//     );
 
-  //   if (availableJetskisCount >= jetskiCount) {
-  //     availabilitySlots.push({
-  //       start_time: slot.start.toTimeString().slice(0, 5),
-  //       end_time: slot.end.toTimeString().slice(0, 5),
-  //       available_jetskis: availableJetskisCount,
-  //     });
-  //     currentSlotIndex += Math.floor(60 / 5);
-  //   } else {
-  //     currentSlotIndex += 1;
-  //   }
-  // }
-  // return availabilitySlots;
+//     return isOverlapping;
+//   });
+
+//   const locationFilteredReservations = overlappingReservations.filter((reservation) => {
+//     return !location || reservation.reservation_jetski_list.some(jetski => jetski.jetski_location_id === location);
+//   });
+
+//   const reservedJetskis = locationFilteredReservations.reduce((count, reservation) => {
+//     return count + reservation.reservation_jetski_list.length;
+//   }, 0);
+
+//   const availableJetskisCount = numberOfAvailableVehicles - reservedJetskis;
+
+//   if (availableJetskisCount >= jetskiCount) {
+//     availabilitySlots.push({
+//       start_time: slot.start.toTimeString().slice(0, 5),
+//       end_time: slot.end.toTimeString().slice(0, 5),
+//       available_jetskis: availableJetskisCount,
+//     });
+//     currentSlotIndex += Math.floor(60 / 5);
+//   } else {
+//     currentSlotIndex += 1;
+//   }
+// }
+// return availabilitySlots;
 // };
--- components/jetski/dashboard.tsx
+++ components/jetski/dashboard.tsx
@@ -1,22 +1,24 @@
-"use client";
+'use client';
 
-import { useEffect, useState } from "react";
-import { Jetski, Location } from "@prisma/client";
-import { listLocation } from "@/actions/listLocations";
-import Spinner from "../ui/spinner";
-import AvailabilityFormModal from "../modal/availabilityForm";
-import { Button } from "../ui/button";
-import ReservationCard from "@/components/ui/reservationcard";
-import { Menu, MenuItem } from "@mui/material";
-import { listJetskisByLocation } from "@/actions/listJetskisByLocation";
-import JetskiReservationCard from "../ui/jetskireservationcard";
-import { listReservationsByDate } from "@/actions/listReservationsForDate";
-import { ExtendedReservation } from "@/types";
+import { useEffect, useState } from 'react';
+import { Jetski, Location } from '@prisma/client';
+import { listLocation } from '@/actions/listLocations';
+import Spinner from '../ui/spinner';
+import AvailabilityFormModal from '../modal/availabilityForm';
+import { Button } from '../ui/button';
+import ReservationCard from '@/components/ui/reservationcard';
+import { Menu, MenuItem } from '@mui/material';
+import { listJetskisByLocation } from '@/actions/listJetskisByLocation';
+import JetskiReservationCard from '../ui/jetskireservationcard';
+import { listReservationsByDate } from '@/actions/listReservationsForDate';
+import { ExtendedReservation } from '@/types';
 
 export const DashboardPage = () => {
   const [locations, setLocations] = useState<Location[]>([]);
   const [reservations, setReservations] = useState<ExtendedReservation[]>([]);
-  const [selectedLocation, setSelectedLocation] = useState<Location | null>(null);
+  const [selectedLocation, setSelectedLocation] = useState<Location | null>(
+    null
+  );
   const [jetskis, setJetskis] = useState<Jetski[]>([]);
   const [error, setError] = useState<string | undefined>(undefined);
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -29,14 +31,14 @@ export const DashboardPage = () => {
         if (locationData) setLocations(locationData);
         const currentDate = new Date();
         currentDate.setHours(currentDate.getHours());
-        console.log(currentDate)
+        console.log(currentDate);
         const reservationsData = await listReservationsByDate(currentDate);
         if (reservationsData) {
           setReservations(reservationsData);
         }
-        console.log(reservationsData)
+        console.log(reservationsData);
       } catch (error) {
-        setError("Failed to fetch locations");
+        setError('Failed to fetch locations');
         console.error(error);
       }
     };
@@ -48,10 +50,12 @@ export const DashboardPage = () => {
     if (selectedLocation) {
       const fetchJetskis = async () => {
         try {
-          const jetskiData = await listJetskisByLocation(selectedLocation.location_id);
+          const jetskiData = await listJetskisByLocation(
+            selectedLocation.location_id
+          );
           if (jetskiData) setJetskis(jetskiData);
         } catch (error) {
-          setError("Failed to fetch jetskis");
+          setError('Failed to fetch jetskis');
           console.error(error);
         }
       };
@@ -96,7 +100,11 @@ export const DashboardPage = () => {
               {jetskis.length > 0 ? (
                 jetskis.map((jetski) => (
                   <div key={jetski.jetski_id} className="m-4">
-                    <JetskiReservationCard jetski={jetski} reservations={reservations} locations={locations}/>
+                    <JetskiReservationCard
+                      jetski={jetski}
+                      reservations={reservations}
+                      locations={locations}
+                    />
                   </div>
                 ))
               ) : (
@@ -119,14 +127,23 @@ export const DashboardPage = () => {
         )}
       </div>
       <div className="absolute top-4 right-8 flex items-center space-x-4">
-        <Button onClick={handleOpenModal}>Check Availability</Button>
+        {/* <Button onClick={handleOpenModal}>Check Availability</Button> */}
         <Button className="w-40" onClick={handleOpenMenu}>
-          {selectedLocation?.location_name || "All locations"}
+          {selectedLocation?.location_name || 'All locations'}
         </Button>
-        <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleCloseMenu}>
-          <MenuItem onClick={() => handleLocationSelect(null)}>All locations</MenuItem>
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleCloseMenu}
+        >
+          <MenuItem onClick={() => handleLocationSelect(null)}>
+            All locations
+          </MenuItem>
           {locations?.map((location) => (
-            <MenuItem key={location.location_id} onClick={() => handleLocationSelect(location.location_id)}>
+            <MenuItem
+              key={location.location_id}
+              onClick={() => handleLocationSelect(location.location_id)}
+            >
               {location.location_name}
             </MenuItem>
           ))}
